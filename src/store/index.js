969a1acebc38f3107cbef8d3e671cdc18f94f6a9
Mike-OxHuge <mikelitoris34@icloud.com>
2021-09-14 22:50:29 +0200

    enabled vuex persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import Vuex from "vuex";
-// import VuexPersistence from "vuex-persist";
+import VuexPersistence from "vuex-persist";
 
-// const vuexLocal = new VuexPersistence({
-//   storage: window.localStorage,
-// });
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 Vue.use(Vuex);
 
@@ -43,5 +43,5 @@ export default new Vuex.Store({
   },
   actions: {},
   modules: {},
-  // plugins: [vuexLocal.plugin],
+  plugins: [vuexLocal.plugin],
 });
